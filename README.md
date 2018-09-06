ec291cbce05ccbaa35e4273399d0b0aeef3cf181
fieldpalmer <gfieldpalmer@gmail.com>
2018-09-05 23:25:07 -0400

    bug fixes with readMe finalized

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # giphyAjaxSite
+
+### Overview
+
+This web app uses the the giphy API to display gifs based on my own personal interests as well as those of the user. 
+
+### Further Explorations
+
+1. Allow users to request new gifs from each topic by clicking again.
+
+2. Create a similar project using OMDB displaying info and movie posters. Potentially link to sell them?
+
+3. Include a 1-click download button for each gif
+
+4. The `favorites` section could use some improvement
+        - A means of removing gifs from favorites would be nice
+        - After returning to the page, the user's new favorites overwrite their old favorites
+        - On small screens, I'd like the gifs to display inline horizontally with a scrollbar
+
+5. When the user adds many gifs to the page, they're more than likely going to have scrolled all the way to the bottom. However, new gifs get loaded at the top. Issues with solving this:
+        -I tried the obvious use of `append` rather than `prepend` and it completely wrecked things. My add to favorites button no longer worked. 
+
+### Author
+    - Field Palmer
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,144 +1,114 @@
-    // Initial array of topic buttons
-    var topics = ['90s', 'Computers', 'Gandalf', 'funny', 'psychadelic', 'animals', 'awesome', 'flight', 'monty python', 'dunk', 'dancing baby', 'yoda'];
-    //blank array to hold thumbnails of favorite gifs
-    var faveGifs = [];
+// Initial array of topic buttons
+var topics = ['90s', 'Computers', 'Gandalf', 'funny', 'psychadelic', 'animals', 'awesome', 'flight', 'monty python', 'dunk', 'dancing baby', 'yoda'];
+//blank array to hold thumbnails of favorite gifs
+var faveGifs = [];
 
-    // Function for displaying buttons with topic data
-    function renderButtons() {
-        // Deleting the gifs prior to adding new gifs
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-        // Looping through the array of movies
-        for (var i = 0; i < topics.length; i++) {
-            // Then dynamicaly generating buttons for each movie in the array
-            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-            var gifButton = $("<button>");
-            // Adding a class of movie-btn to our button
-            gifButton.addClass("gif-btn btn btn-outline-info m-1");
-            // Adding a data-attribute
-            gifButton.attr("data-name", topics[i]);
-            // Providing the initial button text
-            gifButton.text(topics[i]);
-            // Adding the button to the buttons-view div
-            $("#buttons-view").append(gifButton);
-        }   
-    }
+// Function for displaying buttons with topic data
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var gifButton = $("<button>");
+        gifButton.addClass("gif-btn btn btn-outline-info m-1");
+        gifButton.attr("data-name", topics[i]);
+        gifButton.text(topics[i]);
+        $("#buttons-view").append(gifButton);
+    }   
+}
 
-    // displayMovieInfo function re-renders the HTML to display the appropriate content
-    function displayGifInfo () {
-        var topic = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=cLh5y63AXPcyRhP521PdV7eirqOr15bG&limit=10";
-        // Creating an AJAX call for the specific topic button being clicked
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            //Storing the gif data
-            // var results = response.data;
-            for (var j = 0 ; j < response.data.length ; j++) {
-            
-                // Creating a div to hold the gifs
-                var gifSpan = $("<div>");
-                gifSpan.attr("class", "gifs card p-3 align-middle d-inline-block text-white");
-                gifSpan.attr("style", "width: 17rem");
-
-                // Storing & displaying  the gif title
-                var gifTitle = response.data[j].title;
-                var titleText = $("<h5>").text(gifTitle);
-                titleText.attr("class", "card-title pt-2");
-                gifSpan.append(titleText);
-
-                // Storing & displaying the rating data
-                var gifRating = response.data[j].rating;
-                var ratingText = $("<p>").text("Rating: " + gifRating);
-                ratingText.attr("class", "card-text");                
-                gifSpan.append(ratingText);
-
-                var cardBody = $("<div>");
-                cardBody.attr("class", "card-body");
-                gifSpan.wrap(cardBody);
-
-                // Storing & displaying  the gif url
-                var imgUrl = response.data[j].images;
-
-                var gifDisplay = $("<img>");
-                gifDisplay.attr("src", imgUrl.downsized_still.url);
-                gifDisplay.attr("data-still", imgUrl.downsized_still.url);
-                gifDisplay.attr("data-animate", imgUrl.downsized.url);
-                // gifDisplay.attr("data-thumbnail", imgUrl.fixed_height_small.url)
-                gifDisplay.attr("data-state", "still");
-                gifDisplay.attr("class", "gif card-img-top");
-                gifSpan.prepend(gifDisplay);
-
-                //Creating and linking button to add to favorites / local storage
-                var faveButton = $("<input>");
-                faveButton.attr("class", "btn btn-success btn-block");
-                faveButton.attr("id", "faveBTN");
-                faveButton.attr("value", "Favorite");
-                faveButton.attr("data-thumbnail", imgUrl.fixed_height_small.url);
-                gifSpan.append(faveButton);
-
-                //displaying the gif cards in columns
-                gifCardCols = $("<div>");
-                gifCardCols.attr("class", "card-columns")
-                gifSpan.wrap(gifCardCols);
-
-                // Putting the entire gif set above the previous gifs
-                $("#gifs-view").prepend(gifSpan);
-    
-                $("#faveBTN").on("click", function(){
-                            
-                    var newFave = $("<img>");
-                    var faveGifSrc = $(this).attr("data-thumbnail")
-                    faveGifs.push(faveGifSrc);
-                    newFave.attr("src", faveGifSrc);
-                    newFave.attr("class", "m-1");
-                    $(".fave-gifs").prepend(newFave);
-                    localStorage.setItem("gifSrcArray", JSON.stringify(faveGifs));
-        
-                })
-
+// function to pull data from giphy api and display it on our site
+function displayGifInfo () {
+    var topic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=cLh5y63AXPcyRhP521PdV7eirqOr15bG&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        for (var j = 0 ; j < response.data.length ; j++) {
+            // Creating a div to hold the gifs
+            var gifSpan = $("<div>");
+            gifSpan.attr("class", "gifs card p-3 align-middle d-inline-block text-white");
+            gifSpan.attr("style", "width: 17rem");
+            // Storing & displaying  the gif title
+            var gifTitle = response.data[j].title;
+            var titleText = $("<h5>").text(gifTitle);
+            titleText.attr("class", "card-title pt-2");
+            gifSpan.append(titleText);
+            // Storing & displaying the rating data
+            var gifRating = response.data[j].rating;
+            var ratingText = $("<p>").text("Rating: " + gifRating);
+            ratingText.attr("class", "card-text");                
+            gifSpan.append(ratingText);
+            // Creating the div to hold each gif
+            var cardBody = $("<div>");
+            cardBody.attr("class", "card-body");
+            gifSpan.wrap(cardBody);
+            // Storing & displaying  the gif url
+            var imgUrl = response.data[j].images;
+            // Creating & displaying the gif itself with attributes
+            var gifDisplay = $("<img>");
+            gifDisplay.attr("src", imgUrl.downsized_still.url);
+            gifDisplay.attr("data-still", imgUrl.downsized_still.url);
+            gifDisplay.attr("data-animate", imgUrl.downsized.url);
+            // gifDisplay.attr("data-thumbnail", imgUrl.fixed_height_small.url)
+            gifDisplay.attr("data-state", "still");
+            gifDisplay.attr("class", "gif card-img-top");
+            gifSpan.prepend(gifDisplay);
+            //Creating and linking button to add to favorites / local storage
+            var faveButton = $("<input>");
+            faveButton.attr("class", "btn btn-success btn-block");
+            faveButton.attr("id", "faveBTN");
+            faveButton.attr("value", "Favorite");
+            faveButton.attr("data-thumbnail", imgUrl.fixed_height_small.url);
+            gifSpan.append(faveButton);
+            //displaying the gif cards in columns
+            gifCardCols = $("<div>");
+            gifCardCols.attr("class", "card-columns")
+            gifSpan.wrap(gifCardCols);
+            // Putting the entire gif set above the previous gifs
+            $("#gifs-view").prepend(gifSpan);
+            // what happens when you click favorite
+            $("#faveBTN").on("click", function(){
+                var newFave = $("<img>");
+                var faveGifSrc = $(this).attr("data-thumbnail")
+                faveGifs.push(faveGifSrc);
+                newFave.attr("src", faveGifSrc);
+                newFave.attr("class", "m-1");
+                $(".fave-gifs").prepend(newFave);
+                localStorage.setItem("gifSrcArray", JSON.stringify(faveGifs));
+            })
+        }
+        // animating the gifs
+        $(".gif").on("click", function() {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
             }
-
-
-            $(".gif").on("click", function() {
-
-                var state = $(this).attr("data-state");
-                if (state === "still") {
-                  $(this).attr("src", $(this).attr("data-animate"));
-                  $(this).attr("data-state", "animate");
-                } else {
-                  $(this).attr("src", $(this).attr("data-still"));
-                  $(this).attr("data-state", "still");
-                }
-            });
-        });   
-    }
-
-    $("#add-gif").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var newTopic = $("#gif-request").val().trim();
-        // Adding movie from the textbox to our array
-        topics.push(newTopic);
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-    });    
-
-    function showFaves() {
-        var retrieveFaves = JSON.parse(localStorage.getItem("gifSrcArray"));        
-        for (var k = 0 ; k < retrieveFaves.length ; k++) {
-            var newFave = $("<img>");        
-            newFave.attr("src", retrieveFaves[k]);
-            newFave.attr("class", "m-1");
-            $(".fave-gifs").append(newFave);
-        };
-    }
-
-    
-    // Adding a click event listener to all elements with a class of "gif-btn"
-    $(document).on("click", ".gif-btn", displayGifInfo)
-    
-    // Calling the renderButtons function to display the intial buttons
+        });
+    });   
+}
+// new gif button process
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    var newTopic = $("#gif-request").val().trim();
+    topics.push(newTopic);
     renderButtons();
-    showFaves();
+});   
+// showing our favorites pulled from storage 
+function showFaves() {
+    var retrieveFaves = JSON.parse(localStorage.getItem("gifSrcArray"));        
+    for (var k = 0 ; k < retrieveFaves.length ; k++) {
+        var newFave = $("<img>");        
+        newFave.attr("src", retrieveFaves[k]);
+        newFave.attr("class", "m-1");
+        $(".fave-gifs").append(newFave);
+    };
+}
+
+$(document).on("click", ".gif-btn", displayGifInfo)
+
+renderButtons();
+showFaves();
